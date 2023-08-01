f4618112ccb9acdc4f7bd154210cffcc874d5c39
Paul Carlton <paul.carlton@weave.works>
2023-08-01 15:47:43 +0100

    app deploy setup

--- terraform/cluster-templates/leaf-apply-github/main.tf
+++ terraform/cluster-templates/leaf-apply-github/main.tf
@@ -14,6 +14,23 @@ resource "github_repository_file" "global_config" {
   file                = format("%s/%s/global-config.yaml", var.target_path, var.flux_sync_directory)
   content = templatefile("${path.module}/templates/gitrepository.tftpl", {
     name       = "global-config"
+    namespace  = "flux-system"
+    globalGitHubOrg = var.globalGitHubOrg
+    globalGitHubRepo = var.globalGitHubRepo
+    globalGitHubBranch = var.globalGitHubBranch
+  })
+  commit_author       = var.git_commit_author
+  commit_email        = var.git_commit_email
+  commit_message      = var.git_commit_message
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "global_config" {
+  repository          = var.repository_name
+  branch              = var.globalGitHubBranch
+  file                = format("%s/%s/global-config.yaml", var.target_path, var.flux_sync_directory)
+  content = templatefile("${path.module}/templates/gitrepository.tftpl", {
+    name       = "global-config"
     namespace  = var.template_namespace
     globalGitHubOrg = var.globalGitHubOrg
     globalGitHubRepo = var.globalGitHubRepo
