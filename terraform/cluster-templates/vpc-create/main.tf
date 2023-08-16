1038b7d4d820aef3d2a581cc513aef31c806da81
Paul Carlton <paul.carlton@weave.works>
2023-08-16 15:09:07 +0100

    app deploy setup

--- terraform/cluster-templates/vpc-create/main.tf
+++ terraform/cluster-templates/vpc-create/main.tf
@@ -55,7 +55,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "expire_old_logs" {
 
 resource "aws_flow_log" "vpc_flow_logs" {
   iam_role_arn    = aws_iam_role.vpc_flow_logs.arn
-  log_destination = aws_cloudwatch_log_group.vpc
+  log_destination = aws_cloudwatch_log_group.vpc.arn
   traffic_type    = "ALL"
   vpc_id          = module.vpc.vpc_id
 }
