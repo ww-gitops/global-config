a661405b8faa0b260f28d85a88a0c620530e02af
Paul Carlton <paul.carlton@weave.works>
2023-08-16 20:06:53 +0100

    app deploy setup

--- terraform/cluster-templates/s3-create/main.tf
+++ terraform/cluster-templates/s3-create/main.tf
@@ -29,7 +29,16 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encrypt" {
   bucket = aws_s3_bucket.image_versions.id
 }
 
+resource "aws_s3_bucket_ownership_controls" "image_versions" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "private_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.image_versions]
+
   bucket = aws_s3_bucket.image_versions.id
   acl    = "private"
 }
