c1b4a7b6530aee8edf93f0fb4e704d6946229828
Paul Carlton <paul.carlton@weave.works>
2023-08-16 12:56:05 +0100

    app deploy setup

--- terraform/cluster-templates/vpc-create/main.tf
+++ terraform/cluster-templates/vpc-create/main.tf
@@ -15,3 +15,66 @@ module "vpc" {
   public_subnet_count  = var.public_subnet_count
   private_subnet_count = var.private_subnet_count
 }
+
+resource "aws_cloudwatch_log_group" "vpc" {
+  name = var.vpc_name
+}
+
+resource "aws_s3_bucket" "vpc_flow_logs" {
+  bucket = var.vpc_name
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+
+    rule {
+      id      = "expire-old-logs"
+      status  = "Enabled"
+      prefix  = ""
+      enabled = true
+
+      transitions {
+        days          = 30
+        storage_class = "GLACIER"
+      }
+
+      expiration {
+        days = 365
+      }
+    }
+  }
+}
+
+resource "aws_flow_log" "vpc_flow_logs" {
+  iam_role_arn    = aws_iam_role.vpc_flow_logs.arn
+  log_destination = aws_s3_bucket.vpc_flow_logs.arn
+  traffic_type    = "ALL"
+  vpc_id          = module.vpc.vpc_id
+}
+
+resource "aws_iam_role" "vpc_flow_logs" {
+  name = "vpc_flow_logs"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "vpc.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "vpc_flow_logs" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  role       = aws_iam_role.vpc_flow_logs.name
+}
+
