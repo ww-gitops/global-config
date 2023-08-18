98d15e91c9865adbf8424072203a91107836a16f
Paul Carlton <paul.carlton@weave.works>
2023-08-18 16:53:45 +0100

    app deploy setup

--- terraform/modules/aws-ecr-repo-role/variables.tf
+++ terraform/modules/aws-ecr-repo-role/variables.tf
@@ -1,18 +1,3 @@
-variable "repo_name" {
-  type        = string
-  description = "ECR Repo name"
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "resource specific tags"
-  default     = {
-    customer   = "weaveworks-cx"
-    projectGid = "99999"
-    creator    = "paul-carlton@weave.works"
-  }
-}
-
 variable "cluster_name" {
   type        = string
   description = "name of cluster to associate permissions with"
@@ -43,15 +28,12 @@ variable "cluster_oidc_provider_arn" {
 variable "service_account" {
   type        = string
   description = "kubernetes service account"
+  default     = "source-controller"
 }
 
-variable "sa_namespace" {
+variable "namespace" {
   type        = string
   description = "namespace for kubernetes service account"
+  default     = "flux-system"
 }
 
-variable "ecr_role" {
-  type        = string
-  description = "ecr_role created by eks config and assigned to source controller service account"
-  default = ""
-}
