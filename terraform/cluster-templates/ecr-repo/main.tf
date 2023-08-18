94284246e7c4f4a271dcb4145a1c3bf074d87af5
Paul Carlton <paul.carlton@weave.works>
2023-08-18 16:54:59 +0100

    app deploy setup

--- terraform/cluster-templates/ecr-repo/main.tf
+++ terraform/cluster-templates/ecr-repo/main.tf
@@ -14,14 +14,3 @@ resource "aws_ecr_repository" "repo" {
   force_delete         = true
   image_tag_mutability = "IMMUTABLE"
 }
-
-module "aws_ecr_roles" {
-  source                    = "../../modules/aws-ecr-roles"
-  cluster_name              = var.cluster_name
-  cluster_oidc_provider_arn = var.cluster_oidc_provider_arn
-  cluster_oidc_provider_url = var.cluster_oidc_provider_url
-  awsRegion = var.region
-  awsAccountId = var.awsAccountId
-  service_account = var.service_account
-  namespace = var.sa_namespace
-}
