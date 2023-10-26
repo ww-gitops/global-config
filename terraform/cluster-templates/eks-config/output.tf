af8d166542a0381c6afa725ac022b2be8e56802c
Andrew Stangl <andrewstangl@gmail.com>
2023-10-26 09:12:35 +0200

    Disable output as well

--- terraform/cluster-templates/eks-config/output.tf
+++ terraform/cluster-templates/eks-config/output.tf
@@ -4,10 +4,10 @@ output "cluster_path" {
   value       = var.target_path
 }
 
-output "secret_name" {
-  description = "name of secret in vault containing kubeconfig"
-  value       = module.leaf_config.secret_name
-}
+# output "secret_name" {
+#   description = "name of secret in vault containing kubeconfig"
+#   value       = module.leaf_config.secret_name
+# }
 
 output "cluster_oidc_provider_arn" {
   value = data.aws_iam_openid_connect_provider.this.arn
