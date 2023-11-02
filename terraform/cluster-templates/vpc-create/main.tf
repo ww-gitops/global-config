c0e5ba9b7e44cbf0e8f56b034370d47b9aeebcd1
Paul Carlton <paul.carlton@weave.works>
2023-11-02 14:16:07 +0000

    remove flow logs

--- terraform/cluster-templates/vpc-create/main.tf
+++ terraform/cluster-templates/vpc-create/main.tf
@@ -21,65 +21,65 @@ locals {
   bucket_name = format("%s-%s-%s-%s", var.prefix_name, data.aws_caller_identity.current.account_id, var.region, var.vpc_name)
 }
 
-resource "aws_cloudwatch_log_group" "vpc" {
-  name = var.vpc_name
-}
+# resource "aws_cloudwatch_log_group" "vpc" {
+#   name = var.vpc_name
+# }
 
-resource "aws_s3_bucket" "vpc_flow_logs" {
-  bucket = local.bucket_name
-  acl    = "private"
+# resource "aws_s3_bucket" "vpc_flow_logs" {
+#   bucket = local.bucket_name
+#   acl    = "private"
 
-  versioning {
-    enabled = true
-  }
-}
+#   versioning {
+#     enabled = true
+#   }
+# }
 
-resource "aws_s3_bucket_lifecycle_configuration" "expire_old_logs" {
-  bucket = aws_s3_bucket.vpc_flow_logs.id
+# resource "aws_s3_bucket_lifecycle_configuration" "expire_old_logs" {
+#   bucket = aws_s3_bucket.vpc_flow_logs.id
 
-  rule {
-    id      = "expire-old-logs"
-    status  = "Enabled"
+#   rule {
+#     id      = "expire-old-logs"
+#     status  = "Enabled"
 
-    transition {
-      days          = 30
-      storage_class = "GLACIER"
-    }
+#     transition {
+#       days          = 30
+#       storage_class = "GLACIER"
+#     }
 
-    expiration {
-      days = 365
-    }
-  }
-}
+#     expiration {
+#       days = 365
+#     }
+#   }
+# }
 
 
-resource "aws_flow_log" "vpc_flow_logs" {
-  iam_role_arn    = aws_iam_role.vpc_flow_logs.arn
-  log_destination = aws_cloudwatch_log_group.vpc.arn
-  traffic_type    = "ALL"
-  vpc_id          = module.vpc.vpc_id
-}
+# resource "aws_flow_log" "vpc_flow_logs" {
+#   iam_role_arn    = aws_iam_role.vpc_flow_logs.arn
+#   log_destination = aws_cloudwatch_log_group.vpc.arn
+#   traffic_type    = "ALL"
+#   vpc_id          = module.vpc.vpc_id
+# }
 
-resource "aws_iam_role" "vpc_flow_logs" {
-  name = "${local.bucket_name}"
+# resource "aws_iam_role" "vpc_flow_logs" {
+#   name = "${local.bucket_name}"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Sid = ""
-        Principal = {
-          Service = "vpc-flow-logs.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Sid = ""
+#         Principal = {
+#           Service = "vpc-flow-logs.amazonaws.com"
+#         }
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "vpc_flow_logs" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-  role       = aws_iam_role.vpc_flow_logs.name
-}
+# resource "aws_iam_role_policy_attachment" "vpc_flow_logs" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+#   role       = aws_iam_role.vpc_flow_logs.name
+# }
 
