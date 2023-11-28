4994ae74bbdba81169b3663c1c0430b1cfa39a14
Paul Carlton <paul.carlton@weave.works>
2023-11-28 11:39:06 +0000

    New remote state tf

---
+++ terraform/remote-state/main.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = var.awsRegion
+
+  default_tags {
+    tags = merge({
+      source  = "Terraform Managed"
+      Name = var.bucket_name
+    }, var.tags)
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "s3_versioning" {
+  versioning_configuration {
+    status = "Enabled"
+  }
+  bucket = aws_s3_bucket.terraform_state.id
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encrypt" {
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+  bucket = aws_s3_bucket.terraform_state.id
+}
+
+resource "aws_s3_bucket_acl" "private_bucket" {
+  bucket = aws_s3_bucket.terraform_state.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "tf-state-lock" {
+  name           = var.bucket_name
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ terraform/remote-state/variables.tf
@@ -0,0 +1,27 @@
+variable "bucket_name" {
+  type        = string
+  description = "S3 Bucket name"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "resource specific tags"
+  default     = {
+    customer   = "weaveworks-cx"
+    projectGid = "99999"
+    creator    = "paul-carlton@weave.works"
+  }
+}
+
+variable "awsAccountId" {
+  type        = string
+  description = "AWS account id"
+  default     = ""
+}
+
+variable "awsRegion" {
+  type        = string
+  description = "AWS region"
+  default     = ""
+}
+
