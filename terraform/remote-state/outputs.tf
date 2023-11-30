1adc4995d5f7e9fe5d20837245bbce4024adee0f
Paul Carlton <paul.carlton@weave.works>
2023-11-30 17:01:27 +0000

    fix remote tf

--- terraform/remote-state/outputs.tf
+++ terraform/remote-state/outputs.tf
@@ -1,7 +1,6 @@
-output "created" {
-  value = {
-    "bucket_id"    = aws_s3_bucket.terraform_state.id
-    "dynamo_table_id" = aws_dynamodb_table.tf-state-lock.id
-  }
+output "bucket_id" {
+  value = aws_s3_bucket.terraform_state.id
 }
-
+output "dynamo_table_id" {
+  value = aws_dynamodb_table.tf-state-lock.id
+}
