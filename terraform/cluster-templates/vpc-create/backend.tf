4b582bdfc6da742a92d442053d3e48d12c933012
Paul Carlton <paul.carlton@weave.works>
2023-11-30 16:58:42 +0000

    fix remote tf

--- terraform/cluster-templates/vpc-create/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ww-prc-mac-749339757188-eu-west-1-tf-state"
-    key            = "ww-paulc-mac/test-one/vpc/t1-v1/terraform.tfstate"
-    region         = "eu-west-1"
-    encrypt        = true
-    dynamodb_table = "ww-prc-mac-749339757188-eu-west-1-tf-state"
-  }
-}
